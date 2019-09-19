f61b817642f09aa1919f6f996c37bb6cabb008ff
Worome <41673409+Worome@users.noreply.github.com>
2019-09-19 12:31:41 +0200

    Creando spinner item para cargos

--- app/src/main/java/com/trianacodes/script/utahapp/Socios.java
+++ app/src/main/java/com/trianacodes/script/utahapp/Socios.java
@@ -2,15 +2,28 @@ package com.trianacodes.script.utahapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class Socios extends AppCompatActivity {
 
+    private String CargosArray [] = {"Presidente", "Vicepresidente", "Tesorero", "Secretario", "Vocal"};
+    private Spinner SeleccionCargos;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_socios);
         getSupportActionBar().setDisplayShowHomeEnabled(true);
         getSupportActionBar().setIcon(R.mipmap.ic_launcher);
 
+        SeleccionCargos = findViewById(R.id.Cargo);
+
+        ArrayAdapter<String> adaptador_Cargos = new ArrayAdapter<String>(this,
+                android.R.layout.simple_spinner_item,CargosArray);
+        SeleccionCargos.setAdapter(adaptador_Cargos);
+
     }
+    
 }
